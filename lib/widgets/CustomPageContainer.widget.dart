0cba5f3811bcdbdb6638596f052ec096d2139763
Gabriellimmaa <42157830+Gabriellimmaa@users.noreply.github.com>
2023-11-15 20:04:18 -0300

    fix/layout-custom-page-container

--- lib/widgets/CustomPageContainer.widget.dart
+++ lib/widgets/CustomPageContainer.widget.dart
@@ -4,12 +4,13 @@ class CustomPageContainer extends Container {
   CustomPageContainer(
       {Widget? child, double? height, EdgeInsetsGeometry? padding})
       : super(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20), topRight: Radius.circular(20))),
-          height: height,
-          child: child,
-          padding: padding,
-        );
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20))),
+            height: height,
+            child: child,
+            padding: padding,
+            margin: EdgeInsets.only(top: 10));
 }
